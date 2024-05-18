4d20bfd68e86a680948a0dcff1654df4862156e4
Lance_Sabatin <sabatinlance228@gmail.com>
2024-05-19 01:16:15 +0800

    sakit mo na

---
+++ about/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <section className="hero">
+      <div className="content">
+        <h1 className="title">ABOUT</h1>
+        <p className="description">
+          Whether it's every generation of your family or every department in your company, Su Casa Garden Resort makes it easy to get people together. Su Casa offers you immediate access to amenities such as an outdoor adult/kiddie pool, air conditioned videoke room furnished with superior home theater system, pavilion perfect for family and company gatherings such as reunions, birthdays, weddings, team buildings, and so on, two air conditioned bedrooms (with double beds), shower rooms, kitchen with stove and refrigerator, outdoor grill, gazebo and a Koi pond in a serenely landscaped garden.
+        </p>
+        <a href="#" className="overview-link">Overview</a>
+      </div>
+    </section>
+  );
+}
+
+export default App;
